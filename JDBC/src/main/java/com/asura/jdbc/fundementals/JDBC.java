fcdf4ff69d9ef7e19fc85783e3fd07d65a8d5995
dheer <dheer@192.168.1.7>
2017-05-22 02:47:15 -0500

    jdbc program

--- JDBC/src/main/java/com/asura/jdbc/fundementals/JDBC.java
+++ JDBC/src/main/java/com/asura/jdbc/fundementals/JDBC.java
@@ -1,11 +1,23 @@
 package com.asura.jdbc.fundementals;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 /**
  *
  * @author Dheeraj Kumar Reddy Kotha <dheerajkumarreddykotha@gmail.com>
  */
 public class JDBC {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SQLException {
+        Connection conn = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:xe", "hr", "Ammanana143");
+
+        System.out.println("Connection established successfully");
+//        Statement statement = conn.createStatement();
+//        ResultSet resultset = statement.execute("select ");
+
         
     }
 }
@@ -19,10 +31,12 @@ Notes:
 
 SETUP:
 Step 1: Load an Register JDBC Driver
-class.forName("oracle.jdbc.OracleDriver"); ----> This oracle driver is present in ojdbc.jar
+Loading: class.forName("oracle.jdbc.OracleDriver"); ----> This oracle driver is present in ojdbc.jar
+Registering: Inside OracleDriver class we have a static method which registers the driver.
 
 Step 2: Establish connection
 Connection conn = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:Xe","user","password");
+From java 6, we do not need step 1, because while performing step 2, based on url driver class will be loaded.
 
 Step 3: Preparing statement to carry  request to database
 Statement st = conn.createStatement();
@@ -38,5 +52,10 @@ while(rs.next()){
 
 Step 6: Close the connection
 conn.close();
+
+Driver Manager: This class keeps track of the list of drivers available and handles establishing connection between database and appropriate driver.
+Driver:  This interface handles the communications with the database server. 
+Statement: It is an object to carry information to the database
+Resultset: It is an object which holds the data retreived from the database.
 
 */
