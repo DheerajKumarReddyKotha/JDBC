9c9a2172d804fd39f530d562a4b8c6b12131ca7f
dheer <dheer@192.168.1.7>
2017-05-22 00:24:37 -0500

    Added notes about JDBC

--- JDBC/src/main/java/com/asura/jdbc/fundementals/JDBC.java
+++ JDBC/src/main/java/com/asura/jdbc/fundementals/JDBC.java
@@ -9,3 +9,34 @@ public class JDBC {
         
     }
 }
+/*
+Notes:
+1. JDBC is an API that provides connectivity between java application and database.
+2. JDBC Driver class converts java calls to database and database calls to java.
+3. These drivers are different for different databases.
+4. Statement object is used to carry the query from java application to database.
+5. The output after executing statement object is known as result, which is a result set.
+
+SETUP:
+Step 1: Load an Register JDBC Driver
+class.forName("oracle.jdbc.OracleDriver"); ----> This oracle driver is present in ojdbc.jar
+
+Step 2: Establish connection
+Connection conn = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:Xe","user","password");
+
+Step 3: Preparing statement to carry  request to database
+Statement st = conn.createStatement();
+
+Step 4: Executing query and Assigning result
+ResultSet rs = st.execute("sql query");
+
+Step 5: Processing result set
+while(rs.next()){
+    rs.getString(item);
+
+}
+
+Step 6: Close the connection
+conn.close();
+
+*/
